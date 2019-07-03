2e4dce757224d1a8fe6e227055735798e304de8a
abbl <patrykrzucidlo1@gmail.com>
2019-07-03 17:57:37 +0200

    Generated project with Spring initializr

---
+++ src/main/java/ovh/abbl/artiststack/ArtiststackApplication.java
@@ -0,0 +1,13 @@
+package ovh.abbl.artiststack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtiststackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtiststackApplication.class, args);
+	}
+
+}
